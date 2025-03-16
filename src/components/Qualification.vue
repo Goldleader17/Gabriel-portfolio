8512ba1e6795427eb48081aedb74ad13e40de378
Goldleader17 <134707872+Goldleader17@users.noreply.github.com>
2025-03-17 00:24:54 +0100

    Add files via upload

---
+++ src/components/Qualification.vue
@@ -0,0 +1,267 @@
+<script>
+    import Sourcedware from './Qualification/Sourcedware.vue';
+    import Gigabyte from './Qualification/Gigabyte.vue';
+    import University from './Qualification/University.vue';
+    import Internship from './Qualification/Internship.vue';
+    import Training from './Qualification/Training.vue';
+
+    export default{
+        components: { Sourcedware, Gigabyte, University, Internship, Training },
+
+        data(){
+            return{
+                sourcedware: false,
+                gigabyte: false,
+                university: false,
+                internship: false,
+                training: false,
+                //For the Animation
+                sourcedwareVisible: false,
+                gigabyteVisible: false,
+                universityVisible: false,
+                internshipVisible: false,
+                trainingVisible: false,
+                //checking if the class is applied
+                sourcedwareClassApplied: false,
+                gigabyteClassApplied: false,
+                universityClassApplied: false,
+                internshipClassApplied: false,
+                trainingClassApplied: false,
+            }
+        },
+
+        mounted() {
+            window.addEventListener('scroll', this.handleScroll);
+            this.handleScroll();
+        },
+
+        beforeDestroy() {
+            window.removeEventListener('scroll', this.handleScroll);
+        },
+
+        methods: {
+            toggleSourcedware(){
+                this.sourcedware = !this.sourcedware;
+            },
+            toggleGigabyte(){
+                this.gigabyte = !this.gigabyte;
+            },
+            toggleUniversity(){
+                this.university = !this.university;
+            },
+            toggleInternship(){
+                this.internship = !this.internship;
+            },
+            toggleTraining(){
+                this.training = !this.training;
+            },
+            //THIS FOR THE ANIMATION
+            handleScroll() {
+                if (!this.sourcedwareClassApplied) {
+                    const sourcedware = document.getElementById('sourcedware');
+                    const sourcedwareRect = sourcedware.getBoundingClientRect();
+                    if (sourcedwareRect.top < window.innerHeight && sourcedwareRect.bottom >= 0) {
+                        this.sourcedwareVisible = true;
+                    } else {
+                        this.sourcedwareVisible = false;
+                    }
+                    if (this.sourcedwareVisible) {
+                        this.sourcedwareClassApplied = true;
+                    }
+                }
+
+                if (!this.gigabyteClassApplied) {
+                    const framework = document.getElementById('gigabyte');
+                    const gigabyteRect = gigabyte.getBoundingClientRect();
+                    if (gigabyteRect.top < window.innerHeight && gigabyteRect.bottom >= 0) {
+                        this.gigabyteVisible = true;
+                    } else {
+                        this.gigabyteVisible = false;
+                    }
+                    if (this.gigabyteVisible) {
+                        this.gigabyteClassApplied = true;
+                    }
+                }
+
+                if (!this.universityClassApplied) {
+                    const university = document.getElementById('university');
+                    const universityRect = university.getBoundingClientRect();
+                    if (universityRect.top < window.innerHeight && universityRect.bottom >= 0) {
+                        this.universityVisible = true;
+                    } else {
+                        this.universityVisible = false;
+                    }
+                    if (this.universityVisible) {
+                        this.universityClassApplied = true;
+                    }
+                }
+
+                if (!this.internshipClassApplied) {
+                    const internship = document.getElementById('internship');
+                    const internshipRect = internship.getBoundingClientRect();
+                    if (internshipRect.top < window.innerHeight && internshipRect.bottom >= 0) {
+                        this.internshipVisible = true;
+                    } else {
+                        this.internshipVisible = false;
+                    }
+                    if (this.internshipVisible) {
+                        this.internshipClassApplied = true;
+                    }
+                }
+
+                if (!this.trainingClassApplied) {
+                    const training = document.getElementById('training');
+                    const trainingRect = training.getBoundingClientRect();
+                    if (trainingRect.top < window.innerHeight && trainingRect.bottom >= 0) {
+                        this.trainingVisible = true;
+                    } else {
+                        this.trainingVisible = false;
+                    }
+                    if (this.trainingVisible) {
+                        this.trainingClassApplied = true;
+                    }
+                }
+            }
+        }
+    }
+</script>
+<template>
+    <div id="qualification" class="body-con">
+        <!--modal cards-->
+        <div v-if="sourcedware">
+            <Sourcedware @change="toggleSourcedware"></Sourcedware>
+        </div>
+        <div v-if="gigabyte">
+            <Gigabyte @change="toggleGigabyte"></Gigabyte>
+        </div>
+        <div v-if="university">
+            <University @change="toggleUniversity"></University>
+        </div>
+        <div v-if="internship">
+            <Internship @change="toggleInternship"></Internship>
+        </div>
+        <div v-if="training">
+            <Training @change="toggleTraining"></Training>
+        </div>
+
+
+
+        <!--qualification-->
+        <div class="body-subcon text-gray-500 dark:text-white">
+            <!--Title-->
+            <div class="gen-header-con">
+                <div class="gen-header-text">Qualification</div>
+                <div>My Personal Journey</div>
+            </div>
+
+
+
+            <!--Work Experience-->
+            <div class="w-full mx-auto">
+                <div class=" flex justify-between items-center gap-4 w-fit mx-auto my-8">
+                    <span>
+                        <span class="icon icon-[material-symbols--home-work-outline]"></span>
+                    </span>
+                    <span class=" text-indigo-700 text-2xl font-bold">Work Experience</span>
+                </div>
+
+
+                <!--work experience body-->
+                <div class="general-qualification-con">
+                    <!--experience one-->
+                    <div class="qualification-con">
+                        <div  id="sourcedware" :class="{ 'animate-grow': sourcedwareVisible }">
+                            <div class="qualification-header">Sourcedware</div>
+                            <div class="qualification-description">
+                                Full-Stack & Front-End Developer.
+                            </div>
+                            <div class="qualification-date">
+                                <span>
+                                    <span class="icon-[solar--calendar-outline]"></span>
+                                </span>
+                                <span class="text-sm">
+                                    2021 - PRESENT
+                                </span>
+                            </div>
+                            <div class="view-more-con" @click="toggleSourcedware">
+                                <span class="view-more">View More</span>
+                                <span class="icon icon-[uil--arrow-right]"></span>
+                            </div>
+                        </div>
+                        <div class="progress-circle -top-2"></div>
+                        <div class="progress-circle bottom-0"></div>
+                    </div>
+
+                    <!--empty divs-->
+                    <div></div>
+
+                    <div class="border-r border-indigo-700 relative">
+                        <div class="progress-circle bottom-0"></div>
+                    </div>
+
+                    <!--experience two-->
+                    <div class="pl-5">
+                        <div id="gigabyte" :class="{ 'animate-grow': gigabyteVisible }">
+                            <div class="qualification-header">Gigabyte Developers</div>
+                            <div class="qualification-description">                     
+                                Software Engineer & Full-Stack Specialist (Contract).
+                            </div>
+                            <div class="qualification-date">
+                                <span>
+                                    <span class="icon-[solar--calendar-outline]"></span>
+                                </span>
+                                <span class="text-sm">
+                                    2022 - 2024
+                                </span>
+                            </div>
+                            <div class="view-more-con" @click="toggleGigabyte">
+                                <span class="view-more">View More</span>
+                                <span class="icon icon-[uil--arrow-right]"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <!--Education-->
+            <div class="w-full mx-auto">
+                <div class="flex justify-between gap-4 w-fit mx-auto my-8 pt-10">
+                    <span class="icon icon-[fluent--hat-graduation-20-regular]"></span>
+                    <span class="text-indigo-700 text-2xl font-bold">Education</span>
+                </div>
+
+
+                <!--Education Body-->
+                <div class="general-qualification-con">
+                    <!--education one-->
+                    <div class="qualification-con">
+                        <div id="university" :class="{ 'animate-grow': universityVisible }">
+                            <div class="qualification-header">BSc in Software Development</div>
+                            <div class="qualification-description">University of Belgrade, Belgrade, Serbia</div>
+                            <div class="qualification-date">
+                                <span>
+                                    <span class="icon-[solar--calendar-outline]"></span>
+                                </span>
+                                <span class="text-sm">
+                                    2017 – 2020
+                                </span>
+                            </div>
+                            <div class="view-more-con">
+                                <span class="view-more" @click="toggleUniversity">View More</span>
+                                <span class="icon icon-[uil--arrow-right]"></span>
+                            </div>
+                        </div>
+                        <div class="progress-circle -top-2"></div>
+                        <div class="progress-circle bottom-0"></div>
+                    </div>
+
+                    <!--empty divs-->
+                    <div></div>
+                    <div class="qualification-con"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/Skills.vue
@@ -0,0 +1,203 @@
+<script>
+    import Frontend from './Skills/Frontend.vue';
+    import Backend from './Skills/Backend.vue';
+    import DevOpsTools from './Skills/DevOpsTools.vue';
+    import Databases from './Skills/Databases.vue';
+    import Testing from './Skills/TestingOptimization.vue';
+    import Blockchain from './Skills/Blockchain.vue'
+    export default{
+        components: { Frontend, Backend, DevOpsTools, Databases,Testing, Blockchain },
+
+        data(){
+            return{
+                frontend: false,
+                backend: false,
+                devops: false,
+                databases: false,
+                testing: false,
+                blockchain: false,
+            }
+        }
+    }
+</script>
+<template>
+    <div class="body-con" id="skills">
+        <div class="body-subcon text-black dark:text-white">
+            <!--Title-->
+            <div class="gen-header-con mb-10">
+                <div class="gen-header-text">Skills</div>
+                <div class=" font-semibold text-gray-600 dark:text-white">My Technical Level</div>
+            </div>
+
+            <!--Details-->
+            <div class="block sm:grid md:grid-cols-2 gap-12 lg:gap-x-40 mx-auto w-fit">
+                <!--Frontend Development-->
+                <div class="skills">
+                    <!--Frontend Development Header-->
+                    <div class="skills-header" @click="frontend = !frontend">
+                        <div class="skills-icon">
+                            <span class="icon icon-[fa--code]"></span>
+                        </div>
+                        <div class="skills-text">Frontend Development</div>
+                        <div class="skills-display">
+                            <div v-if="!frontend">
+                                <span class="icon icon-[uil--angle-down]"></span>
+                            </div>
+                            <div v-if="frontend">
+                                <span class="icon icon-[uil--angle-up]"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Frontend Development Body-->
+                    <Transition name="skill" mode="in-out">
+                        <div v-if="frontend" class="skills-body">
+                            <Frontend></Frontend>
+                        </div>
+                    </Transition>
+                </div>
+
+                <!--Backend Development-->
+                <div class="skills">
+                    <!--Backend Development Header-->
+                    <div class="skills-header" @click="backend = !backend">
+                        <div class="skills-icon">
+                            <span class="icon icon-[carbon--deployment-pattern]"></span>
+                        </div>
+                        <div class="skills-text">Backend Development</div>
+                        <div class="skills-display">
+                            <div v-if="!backend">
+                                <span class="icon icon-[uil--angle-down]"></span>
+                            </div>
+                            <div v-if="backend">
+                                <span class="icon icon-[uil--angle-up]"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Backend Development Body-->
+                    <Transition name="skill" mode="in-out">
+                        <div v-if="backend" class="skills-body">
+                            <Backend></Backend>
+                        </div>
+                    </Transition>
+                </div>
+
+                <!--DevOps & Tools-->
+                <div class="skills">
+                    <!--DevOps & Tools header-->
+                    <div class="skills-header" @click="devops = !devops">
+                        <div class="skills-icon">
+                            <span class="icon icon-[ant-design--deployment-unit-outlined]"></span>
+                        </div>
+                        <div class="skills-text">DevOps & Tools</div>
+                        <div class="skills-display">
+                            <div v-if="!devops">
+                                <span class="icon icon-[uil--angle-down]"></span>
+                            </div>
+                            <div v-if="devops">
+                                <span class="icon icon-[uil--angle-up]"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Infrasructural Tools Body-->
+                    <Transition name="skill" mode="in-out">
+                        <div v-if="devops" class="skills-body">
+                            <DevOpsTools></DevOpsTools>
+                        </div>
+                    </Transition>
+                </div>
+
+                <!--Databases-->
+                <div class="skills">
+                    <!--Databases Header-->
+                    <div class="skills-header" @click="databases = !databases">
+                        <div class="skills-icon">
+                            <span class="icon icon-[streamline--industry-innovation-and-infrastructure-solid]"></span>
+                        </div>
+                        <div class="skills-text">Databases</div>
+                        <div class="skills-display">
+                            <div v-if="!databases">
+                                <span class="icon icon-[uil--angle-down]"></span>
+                            </div>
+                            <div v-if="databases">
+                                <span class="icon icon-[uil--angle-up]"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Databases Body-->
+                    <Transition name="skill" mode="in-out">
+                        <div class="skills-body" v-if="databases">
+                            <Databases></Databases>
+                        </div>
+                    </Transition>
+                </div>
+
+                <!--Testing & Optimization-->
+                <div class="skills">
+                    <!--Testing & Optimization Header-->
+                    <div class="skills-header" @click="testing = !testing">
+                        <div class="skills-icon">
+                            <span class="icon icon-[fluent--developer-board-16-regular]"></span>
+                        </div>
+                        <div class="skills-text">Testing & Optimization</div>
+                        <div class="skills-display">
+                            <div v-if="!testing">
+                                <span class="icon icon-[uil--angle-down]"></span>
+                            </div>
+                            <div v-if="testing">
+                                <span class="icon icon-[uil--angle-up]"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Testing & Optimization Body-->
+                    <Transition name="skill" mode="in-out">
+                        <div class="skills-body" v-if="testing">
+                            <Testing></Testing>
+                        </div>
+                    </Transition>
+                </div>
+
+                <!--Blockchain & Smart Contracts-->
+                <div class="skills">
+                    <!--Blockchain & Smart Contracts Header-->
+                    <div class="skills-header" @click="blockchain = !blockchain">
+                        <div class="skills-icon">
+                            <span class="icon icon-[eos-icons--blockchain]"></span>
+                        </div>
+                        <div class="skills-text">Blockchain & Smart Contracts</div>
+                        <div class="skills-display">
+                            <div v-if="!blockchain">
+                                <span class="icon icon-[uil--angle-down]"></span>
+                            </div>
+                            <div v-if="blockchain">
+                                <span class="icon icon-[uil--angle-up]"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Blockchain & Smart Contracts Body-->
+                    <Transition name="skill" mode="in-out">
+                        <div class="skills-body" v-if="blockchain">
+                            <Blockchain></Blockchain>
+                        </div>
+                    </Transition>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+    .skill-enter-active{
+    transition: opacity 0.5s, transform 0.5s;
+    }
+    .skill-leave-active {
+    transition: opacity 0.5s, transform 0.5s;
+    }
+    .skill-leave-to {
+    opacity: 0;
+    transform: translateY(-10%);
+    }
+    .skill-enter-from{
+        opacity: 0;
+        transform: translateY(-10%);
+    }
+</style>
